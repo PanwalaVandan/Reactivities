96c9f368d390d8cd9e44f5ad1b14598927c28e3c
Vandan Panwala <vandanpanwala@gmail.com>
2020-06-18 23:49:03 +0530

    End of Section 8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,68 +1,39 @@
-import React, { useEffect, Fragment, useContext } from "react";
+import React, { Fragment } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { LoadingComponent } from "./LoadingComponent";
-import ActivityStore from '../stores/activityStore';
-import {observer} from 'mobx-react-lite';
-// Removed as implementing react hooks
-// interface IState {
-//   activities: IActivity[];
-// }
+import { observer } from "mobx-react-lite";
+import { Route, withRouter, RouteComponentProps } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+// import { ActivityForm } from "../../features/activities/form/ActivityForm";
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from "../../features/activities/dashboard/details/ActivityDetails";
 
-// class App extends Component<{}, IState> {
-const App = () => {
-  const activityStore = useContext(ActivityStore)
-
-  useEffect(() => {
-    activityStore.loadActivities();
-
-    // telling useEffect about the dependency, that we are using activityStore
-  }, [activityStore]);
-
-  if(activityStore.loadingInitial) 
-  return <LoadingComponent content='Loading activities...' />
-  // Removed as implementing react hooks
-  // readonly state: IState = {
-  //   activities: [],
-  // }
-
-  // componentDidMount() {
-  //   axios
-  //   .get<IActivity[]>('http://localhost:5000/api/activities')
-  //   .then((response) => {
-  //     this.setState({
-  //       activities: response.data
-  //     })
-  //   })
-  // }
-
-  // render() {
-  //   return (
-  //     <div>
-  //       <Header as='h2'>
-  //         <Icon name='plug' />
-  //         <Header.Content>Reactivities</Header.Content>
-  //       </Header>
-  //       <List>
-  //       {this.state.activities.map((activity) => (
-  //             <List.Item key={activity.id}>
-  //               {activity.title}
-  //             </List.Item>
-  //           ))}
-  //       </List>
-  //     </div>
-  //   );
-  // }
+const App: React.FC<RouteComponentProps> = ({ location }) => {
+  
 
   return (
     <Fragment>
-      <NavBar/>
-      <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard/>
-      </Container>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <Fragment>
+            <NavBar />
+            <Container style={{ marginTop: '7em' }}>
+              <Route exact path='/activities' component={ActivityDashboard} />
+              <Route path='/activities/:id' component={ActivityDetails} />
+              <Route
+                key={location.key}
+                path={['/createActivity', '/manage/:id']}
+                component={ActivityForm}
+              />
+            </Container>
+          </Fragment>
+        )}
+      />
     </Fragment>
   );
 };
 
-export default observer(App);
+export default withRouter(observer(App));
